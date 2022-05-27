e339b6d49531a58540a3f0877467d40adad4465a
Nick Douglas <douglas.extern@schuettflix.de>
2022-05-27 14:49:39 +0100

    Login ui

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,8 +2,57 @@ import SiteHeader from "../components/SiteHeader"
 
 const Login = () => {
   return (
-    <main className="mx-auto max-w-7xl">
-      <SiteHeader hideNav={true} />
+    <main className="flex items-center justify-center h-full mx-auto max-w-7xl">
+
+      <div className="flex items-center justify-center min-h-full px-4 py-12 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <SiteHeader hideNav={true} />
+            <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">Sign in to your account</h2>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-orange-400 focus:border-orange-400 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-b-md focus:outline-none focus:ring-orange-400 focus:border-orange-400 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-orange-400 border border-transparent rounded-md group hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-400"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </main>
   )
 }
