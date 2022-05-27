bfc36caeb6803068036442b4d3034542eefc22a4
Nick Douglas <douglas.extern@schuettflix.de>
2022-05-27 09:16:55 +0100

    temp TS fix

--- components/Credits.tsx
+++ components/Credits.tsx
@@ -1,7 +1,8 @@
 import { Data, dataMutation } from "../lib/helpers/dataMutation";
 import List from "./List";
 
-const Credits = ({ credits }) => {
+const Credits = ({ credits }: any) => {
+  console.log(credits)
   const cast = credits.cast.slice(0, 6).map((data: Data) => dataMutation(data, "person"));
   return (
     <div className="mt-5 text-gray-400 mb-9">
