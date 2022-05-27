0077ebce2fa478c047dc00f024cb2b1f67b8bae1
Nick Douglas <douglas.extern@schuettflix.de>
2022-05-27 09:17:03 +0100

    remove console log

--- components/Credits.tsx
+++ components/Credits.tsx
@@ -2,7 +2,7 @@ import { Data, dataMutation } from "../lib/helpers/dataMutation";
 import List from "./List";
 
 const Credits = ({ credits }: any) => {
-  console.log(credits)
+
   const cast = credits.cast.slice(0, 6).map((data: Data) => dataMutation(data, "person"));
   return (
     <div className="mt-5 text-gray-400 mb-9">
